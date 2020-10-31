f0c1e7bcb33ce74415fb6fa0bf93740a90b06971
Serena Wang <serenawa@andrew.cmu.edu>
2020-10-30 20:20:38 -0700

    hw 6A

--- dynam.js
+++ dynam.js
@@ -1,60 +1,86 @@
 //javascript for adding functionality to the shopping cart
 
+
+//get the item
+function itemSave(event){
+    console.log("hi", event.target.alt);
+    localStorage.clear();
+    const item = {"item": event.target.alt};
+    const jsonItem = JSON.stringify(item);
+    localStorage.setItem("yourItem", jsonItem);
+}
+
 //get the color
 function update(event) {
     console.log("hi ", event.target.value);
     const color = {"color": event.target.value};
     const jsonColor = JSON.stringify(color);
-    localStorage.setItem("yourColor", jsonColor)
+    localStorage.setItem("yourColor", jsonColor);
 }
 
 //get the material
 function updatetwo(event) {
     console.log("hi ", event.target.alt);
-    const material = {"material": event.target.class};
+    const material = {"material": event.target.alt};
     const jsonMaterial = JSON.stringify(material);
-    localStorage.setItem("yourMaterial", jsonMaterial)    
+    localStorage.setItem("yourMaterial", jsonMaterial); 
 }
 
 
 //add to cart - just update cart number here?
 function saveItem() {
-   const fullCart = localStorage.getItem("yourColor", "yourMaterial")
-     if (fullCart === null) {
-         return;
+   console.log("saved");
+
+   const yourColor = localStorage.getItem("yourColor");
+   const yourMaterial = localStorage.getItem("yourMaterial");
+   const yourItem = localStorage.getItem("yourItem");
+
+   console.log(yourColor, yourMaterial)
+
+   if (yourColor===null || yourMaterial===null || yourItem===null) {
+    alert("You must pick a color AND material!");
   }
-    else {
-        const savedCart = JSON.parse(fullCart);
-        console.log("saved")
+  else {
+    document.getElementById("quant").style.display = "inline";
+    }
 }
 
 const cartContainer = document.getElementById("cart-items");
 
-function fullCart(parent, color, material) {
+function fullCart(parent, item, color, material) {
     let divContainer = document.createElement("div");
-    divContainer.className = "cart";
+    //divContainer.className = "cart";
     parent.appendChild(divContainer);
 
+    let divItem = document.createElement("p");
+    divItem.innerHTML = "Item: " + item;
+    divContainer.appendChild(divItem);
+
     let divColor = document.createElement("p");
-    divColor.innerHTML = color;
+    divColor.innerHTML = "Color: " + color;
     divContainer.appendChild(divColor);
 
     let divMaterial = document.createElement("p");
-    divMaterial.innerHTML = material;
+    divMaterial.innerHTML = "Material: " + material;
     divContainer.appendChild(divMaterial);
 
+    return divContainer;
 }
 
-//return all the data when the cart is clicked, need to add this function to the cart icon
+//return all the data when the cart is clicked
 function onCartLoad() {
   const yourColor = localStorage.getItem("yourColor");
   const yourMaterial = localStorage.getItem("yourMaterial");
-  if (yourColor === null || yourMaterial===null) {
+  const yourItem = localStorage.getItem("yourItem");
+  if (yourColor===null || yourMaterial===null || yourItem===null) {
     return;
   }
   else {
     const savedColor = JSON.parse(yourColor);
-    fullCart(cartContainer, savedColor.color, savedMaterial.material)
+    const savedMaterial = JSON.parse(yourMaterial);
+    const savedItem = JSON.parse(yourItem);
+    console.log("loaded", savedColor, savedMaterial);
+    fullCart(cartContainer, savedItem.item, savedColor.color, savedMaterial.material);
   }
 }
 
@@ -67,30 +93,3 @@ document.forms.ShoppingList.data.value=localStorage.removeItem(name);
 doShowAll();
 }
 
-
-//function to save information on the item that was selected
-function cartItems(name, quantity, color, stuffing) {
-	let divContainer = document.createElement("div");
-
-	addButton.onclick = function () {
-    	const details = {"name":name, "quantity":quantity, "color":color, "stuffing": stuffing};
-    	const jsonDetails = JSON.stringify(details);
-    	localStorage.setItem("myDetails", jsonDetails);
-	}
-
-  divContainer.appendChild(addButton);
-
-  return divContainer;
-} 
-
-
-function onLoad() {
-  const myDetails = localStorage.getItem("myDetails");
-  if (myDetails === null) {
-    return;
-  }
-  else {
-    const savedDetails = JSON.parse(myDetails);
-    cartItems(savedDetails.name, savedDetails.quantity, savedDetails.color, savedDetails.stuffing);
-  }
-}
